91b2ac8ab0e508454d92c529647db0231005b9b0
Mark O'Beirne <markobeirne00@gmail.com>
2021-02-16 23:30:57 +0000

    Add script to fetch from API
    - Fetch fixture details for Ireland from Rugby Live Data API
    - Comment out to prevent requests

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+
+// List fixtures by team
+/*fetch("https://rugby-live-data.p.rapidapi.com/fixtures-by-team/1817", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "8ce6c9e68emshb1f2c358ed2a7d4p1f5d78jsn5bbc0909ad0d",
+		"x-rapidapi-host": "rugby-live-data.p.rapidapi.com"
+	}
+})
+.then(response => response.json())
+.then(data => console.log(data.results[0])) // Logs next upcoming fixture
+.catch(err => {
+	console.error(err);
+});
+*/
